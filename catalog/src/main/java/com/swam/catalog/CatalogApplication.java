f08bc772a6474a7289c88bf80a988a1f76d84fa2
albe9 <albertom1999@gmail.com>
2024-07-29 19:22:11 +0200

    Setup for dev env (work still in progress)

--- catalog/src/main/java/com/swam/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/swam/catalog/CatalogApplication.java
@@ -2,20 +2,20 @@ package com.swam.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class CatalogApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Add all necessary setup
+
+# Build config-server .jar
+./config-server/mvnw -f ./config-server package 
