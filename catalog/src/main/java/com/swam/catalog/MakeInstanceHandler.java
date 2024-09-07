0b512c5e93312fdb92dcafb2392edd5801f0f892
albe9 <albertom1999@gmail.com>
2024-09-07 12:40:17 +0200

    Added subworkflow support for makeInstance

--- catalog/src/main/java/com/swam/catalog/MakeInstanceHandler.java
+++ catalog/src/main/java/com/swam/catalog/MakeInstanceHandler.java
@@ -1,31 +1,28 @@
 package com.swam.catalog;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qesm.ProductTemplate;
 import com.qesm.WorkflowInstance;
 import com.qesm.WorkflowTemplate;
 import com.swam.commons.intercommunication.ApiTemplateVariable;
 import com.swam.commons.intercommunication.CustomMessage;
-import com.swam.commons.intercommunication.MessageHandler;
 import com.swam.commons.intercommunication.ProcessingMessageException;
 import com.swam.commons.intercommunication.RoutingInstructions.TargetMessageHandler;
+import com.swam.commons.messageHandlers.AbstractBaseHandler;
 import com.swam.commons.mongodb.instance.WorkflowInstanceDTO;
 import com.swam.commons.mongodb.template.WorkflowTemplateDTO;
 import com.swam.commons.mongodb.template.WorkflowTemplateDTORepository;
 
 @Service
-public class MakeInstanceHandler extends MessageHandler {
-
-    private final WorkflowTemplateDTORepository workflowTemplateDTORepository;
+public class MakeInstanceHandler extends AbstractBaseHandler<WorkflowTemplateDTO, ProductTemplate> {
 
     public MakeInstanceHandler(WorkflowTemplateDTORepository workflowTemplateDTORepository) {
-        super(List.of(TargetMessageHandler.ISTANCE_TEMPLATE));
-        this.workflowTemplateDTORepository = workflowTemplateDTORepository;
+        super(List.of(TargetMessageHandler.ISTANCE_TEMPLATE), workflowTemplateDTORepository);
     }
 
     @Override
@@ -55,14 +52,19 @@ public class MakeInstanceHandler extends MessageHandler {
     private void instanceTemplate(CustomMessage context) throws ProcessingMessageException {
         String workflowId = getUriId(context, ApiTemplateVariable.WORKFLOW_ID, true);
 
-        Optional<WorkflowTemplateDTO> resultDTO = workflowTemplateDTORepository.findById(workflowId);
+        // Check if workflow exist
+        if (!workflowRepository.existsById(workflowId)) {
+            throw new ProcessingMessageException("Workflow with workflowId: " + workflowId + " not found", 404);
+        }
 
-        if (resultDTO.isEmpty()) {
-            throw new ProcessingMessageException("ID: " + workflowId + " not present in Catalog DB", 400);
+        // Check if it's requested a subworkflow instead of full workflow
+        WorkflowTemplateDTO workflowDTO = getSubWorkflowIfRequested(context, workflowId);
+        if (workflowDTO == null) {
+            workflowDTO = workflowRepository.findById(workflowId).get();
         }
 
         // TODO: do we need additional sanity checks?
-        WorkflowTemplate workflowTemplate = (WorkflowTemplate) resultDTO.get().convertAndValidate();
+        WorkflowTemplate workflowTemplate = (WorkflowTemplate) workflowDTO.convertAndValidate();
         WorkflowInstance workflowInstance = workflowTemplate.makeInstance();
         WorkflowInstanceDTO workflowInstanceDTO = new WorkflowInstanceDTO(workflowInstance);
         ObjectMapper objectMapper = new ObjectMapper();
